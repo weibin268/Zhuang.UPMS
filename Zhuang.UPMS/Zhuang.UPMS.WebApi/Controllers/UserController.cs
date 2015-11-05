236d947b09da36565b0027fda0cc1fbbc4ff867f
zhuangweibin <binweizhuang@hotmail.com>
2015-11-05 17:42:56 +0800

    add user controler

---
+++ Zhuang.UPMS/Zhuang.UPMS.WebApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Zhuang.Data;
+using Zhuang.Model.Common;
+
+namespace Zhuang.UPMS.WebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        DbAccessor _dba = DbAccessor.Get();
+
+        public SecUser GetUserById(string userId)
+        {
+            return _dba.QueryEntity<SecUser>("select * from sec_user where UserId=#UserId#", new { UserId = userId });
+        }
+    }
+}
