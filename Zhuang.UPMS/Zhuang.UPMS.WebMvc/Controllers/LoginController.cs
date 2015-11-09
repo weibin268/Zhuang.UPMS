79cd01feca2219321f251165322ba177879a6adf
zhuangweibin <binweizhuang@hotmail.com>
2015-11-09 21:08:32 +0800

    sdfgsdf

--- Zhuang.UPMS/Zhuang.UPMS.WebMvc/Controllers/LoginController.cs
+++ Zhuang.UPMS/Zhuang.UPMS.WebMvc/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zhuang.Web.Utility.Images;
 
 namespace Zhuang.UPMS.WebMvc.Controllers
 {
@@ -12,6 +13,16 @@ namespace Zhuang.UPMS.WebMvc.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public FileContentResult GetValidateCode()
+        {
+
+            ValidateCodeHelper vc = new ValidateCodeHelper();
+            string strValidateCode = vc.CreateValidateCode(4);
+            Session["ValidateCode"] = strValidateCode;
+            return File(vc.CreateValidateGraphic(strValidateCode), "image/jpeg");
+
         }
     }
 }
