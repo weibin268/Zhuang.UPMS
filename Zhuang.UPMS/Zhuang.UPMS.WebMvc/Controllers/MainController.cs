d5050a710ed9b4991fc18b3cf378cd4ace984916
zhuangweibin <binweizhuang@hotmail.com>
2015-11-09 23:09:37 +0800

    aa

--- Zhuang.UPMS/Zhuang.UPMS.WebMvc/Controllers/MainController.cs
+++ Zhuang.UPMS/Zhuang.UPMS.WebMvc/Controllers/MainController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zhuang.Data;
+using Zhuang.Web.Utility.EasyUI;
 
 namespace Zhuang.UPMS.WebMvc.Controllers
 {
     public class MainController : Controller
     {
+        DbAccessor _dba = DbAccessor.Get();
+
         // GET: Main
         public ActionResult Index()
         {
@@ -17,7 +21,12 @@ namespace Zhuang.UPMS.WebMvc.Controllers
         public ContentResult GetMenus()
         {
             ContentResult contentResult = new ContentResult();
-            //contentResult.Content = sys_MenuBLL.GetJson4EasyUI("View_Cms_Menu");
+
+            var lsTree = _dba.QueryEntities<TreeModel>(@"SELECT MenuId AS id,ParentId AS parentId,MenuName AS name 
+                                            FROM dbo.Sys_Menu
+                                            WHERE ModuleType='sys'");
+
+            contentResult.Content = Newtonsoft.Json.JsonConvert.SerializeObject(lsTree);
             return contentResult;
         }
     }
