3796cad999a9fe75c21cfac6db0719fcfb51bf6a
zhuangweibin <binweizhuang@hotmail.com>
2015-11-10 11:17:56 +0800

    ggg

--- Zhuang.UPMS/Zhuang.Web.Utility/EasyUI/Models/TreeModel.cs
+++ Zhuang.UPMS/Zhuang.Web.Utility/EasyUI/Models/TreeModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Zhuang.Web.Utility.EasyUI
+namespace Zhuang.Web.Utility.EasyUI.Models
 {
     public enum TreeStateType
     {
