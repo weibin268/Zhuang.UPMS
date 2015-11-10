f6e5fe6f885f5a7be8ef4a0876914d007503993b
zhuangweibin <binweizhuang@hotmail.com>
2015-11-10 21:04:22 +0800

    aaa

--- Zhuang.UPMS/Zhuang.UPMS.WebMvc/Areas/SecuritySettings/Controllers/UserController.cs
+++ Zhuang.UPMS/Zhuang.UPMS.WebMvc/Areas/SecuritySettings/Controllers/UserController.cs
@@ -3,15 +3,92 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Zhuang.Data;
+using Zhuang.Model.Common;
+using Zhuang.Security.Services;
+using Zhuang.Web.Utility.EasyUI.Models;
 
 namespace Zhuang.UPMS.WebMvc.Areas.SecuritySettings.Controllers
 {
     public class UserController : Controller
     {
+        UserService _userService = new UserService();
+        DbAccessor _dba = DbAccessor.Get();
+
         // GET: SecuritySettings/User
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Edit(string id)
+        {
+            SecUser user = new SecUser();
+            if (id != "0")
+            {
+                user = _userService.GetUserById(id);
+            }
+            return View(user);
+        }
+
+        public JsonResult Save(SecUser model)
+        {
+            MyJsonResult mjr = new MyJsonResult();
+
+            using (var dba = DbAccessor.Create())
+            {
+
+                try
+                {
+
+                    if (model.UserId == null)
+                    {
+                        model.UserId = Guid.NewGuid().ToString();
+                        model.RecordStatus = RecordStatus.Active;
+                        dba.Insert<SecUser>(model);
+                    }
+                    else
+                    {
+                        dba.UpdateFields(model, "LoginName", "Password", "Name");
+                    }
+
+                    dba.CommitTran();
+                    mjr.Success = true;
+                    mjr.Message = "保存成功！";
+                }
+                catch (Exception ex)
+                {
+                    dba.RollbackTran();
+                    mjr.Success = false;
+                    mjr.Message = ex.Message;
+                }
+            }
+
+            return Json(mjr);
+        }
+
+        public ContentResult GetList(int rows, int page, string filter)
+        {
+            ContentResult cr = new ContentResult();
+
+
+            string strWhere = "";
+            if (filter != null)
+            {
+                strWhere = string.Format("UserName like '%{0}%' or Name like '%{0}%' or Address like '%{0}%'", filter);
+            }
+
+            DataGridUrlReturnDataModel model = new DataGridUrlReturnDataModel();
+
+            int totalRowCount = 0;
+            var users = _dba.PageQueryEntities<SecUser>("select * from sec_user", "userid", page, rows, out totalRowCount);
+            model.rows = users;
+            model.total = totalRowCount;
+
+            cr.Content = Newtonsoft.Json.JsonConvert.SerializeObject(model);
+
+            return cr;
+
         }
     }
 }
---
+++ Zhuang.UPMS/Zhuang.Web.Utility/EasyUI/Models/DataGridUrlReturnDataModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.Web.Utility.EasyUI.Models
+{
+    public class DataGridUrlReturnDataModel
+    {
+        public int total { get; set; }
+        public object rows { get; set; }
+    }
+}
