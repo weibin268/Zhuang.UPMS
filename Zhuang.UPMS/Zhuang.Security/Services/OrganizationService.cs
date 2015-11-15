43be3fbd71179f6b2612dd0781ef89f517db27fa
zhuangweibin <binweizhuang@hotmail.com>
2015-11-16 00:11:57 +0800

    aaa

---
+++ Zhuang.UPMS/Zhuang.Security/Services/OrganizationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zhuang.Data;
+using Zhuang.Model.Common;
+
+namespace Zhuang.Security.Services
+{
+    public class OrganizationService
+    {
+        DbAccessor _dba = DbAccessor.Get();
+
+        public SecOrganization GetOrganizationById(string menuId)
+        {
+            return _dba.QueryEntity<SecOrganization>("Security.SecOrganization.GetSecOrganizationById", new { MenuId = menuId });
+        }
+    }
+}
--- Zhuang.UPMS/Zhuang.UPMS.WebMvc/Areas/SecuritySettings/Controllers/OrganizationController.cs
+++ Zhuang.UPMS/Zhuang.UPMS.WebMvc/Areas/SecuritySettings/Controllers/OrganizationController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Zhuang.Data;
 using Zhuang.Model.Common;
+using Zhuang.Security;
+using Zhuang.Security.Services;
+using Zhuang.UPMS.WebMvc.App_Code;
 using Zhuang.Web.Utility.EasyUI.Models;
 
 namespace Zhuang.UPMS.WebMvc.Areas.SecuritySettings.Controllers
@@ -12,6 +15,7 @@ namespace Zhuang.UPMS.WebMvc.Areas.SecuritySettings.Controllers
     public class OrganizationController : Controller
     {
         DbAccessor _dba = DbAccessor.Get();
+        OrganizationService _organizationService = new OrganizationService();
 
         // GET: SecuritySettings/Organization
         public ActionResult Index()
@@ -19,11 +23,78 @@ namespace Zhuang.UPMS.WebMvc.Areas.SecuritySettings.Controllers
             return View();
         }
 
+        public ActionResult List()
+        {
+            ViewBag.ParentId = Request.QueryString["ParentId"];
+
+            return View();
+        }
+
+
+        public ActionResult Edit(string id, string ParentId)
+        {
+            ViewBag.ParentName = _dba.ExecuteScalar<string>("SELECT Name FROM dbo.Sec_Organization WHERE OrganizationId=#OrganizationId#",
+                new { OrganizationId = ParentId });
+
+            SecOrganization model = new SecOrganization();
+            model.ParentId = ParentId;
+            if (id != null)
+            {
+                model = _organizationService.GetOrganizationById(id);
+            }
+            return View(model);
+        }
+
+        public JsonResult Save(SecOrganization model)
+        {
+            MyJsonResult mjr = new MyJsonResult();
+
+            using (var dba = DbAccessor.Create())
+            {
+                try
+                {
+                    dba.BeginTran();
+
+                    model.ModifiedById = SecurityContext.Current.User.UserId;
+                    model.ModifiedDate = DateTime.Now;
+
+                    if (model.OrganizationId == null)
+                    {
+                        model.OrganizationId = Guid.NewGuid().ToString();
+                        model.RecordStatus = RecordStatus.Active;
+                        model.CreatedById = SecurityContext.Current.User.UserId;
+                        model.CreationDate = DateTime.Now;
+                        dba.ExecuteNonQuery("Security.Organization.Insert", model);
+                    }
+                    else
+                    {
+                        dba.UpdateFields(model, "Name", "Code",
+                            "MobilePhone",
+                            "ModifiedById", "ModifiedDate");
+                    }
+
+                    dba.CommitTran();
+                    mjr.Success = true;
+                    mjr.Message = "保存成功！";
+                }
+                catch (Exception ex)
+                {
+                    dba.RollbackTran();
+                    mjr.Success = false;
+                    mjr.Message = ex.Message;
+                }
+            }
+
+            return Json(mjr);
+        }
+
+
+
         public ContentResult GetTree()
         {
             ContentResult contentResult = new ContentResult();
 
-            var lsSecOrganization = _dba.QueryEntities<SecOrganization>("SecuritySettings.Menu.GetTree");
+            var lsSecOrganization = _dba.QueryEntities<SecOrganization>("SecuritySettings.Organization.GetTree");
 
             List<TreeModel> lsTree = new List<TreeModel>();
 
@@ -40,6 +111,11 @@ namespace Zhuang.UPMS.WebMvc.Areas.SecuritySettings.Controllers
 
             contentResult.Content = Newtonsoft.Json.JsonConvert.SerializeObject(TreeModel.ToTreeModel(lsTree));
             return contentResult;
+        }
+
+        public ContentResult GetList(int page, int rows)
+        {
+            return EasyUIHelper.GetDataGridPageData("SecuritySettings.Organization.List", "OrganizationId", page, rows);
         }
     }
 }
