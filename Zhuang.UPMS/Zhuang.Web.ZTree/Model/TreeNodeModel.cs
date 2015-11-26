ff1ffa1f9d7135614e3b83fd2060aa29ade9a33f
zhuangweibin <binweizhuang@hotmail.com>
2015-11-26 23:44:17 +0800

    aaaa

---
+++ Zhuang.UPMS/Zhuang.Web.ZTree/Model/TreeNodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.Web.ZTree.Model
+{
+    public class TreeNodeModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public List<TreeNodeModel> children { get; set; }
+        public bool @checked { get; set; }
+        public bool open { get; set; }
+        public string url { get; set; }
+
+    }
+}
