10f5835d47b69f1597506b24aabb2a7bc2ab2745
zhuangweibin <binweizhuang@hotmail.com>
2015-12-15 17:22:05 +0800

    aaaa

---
+++ Zhuang.UPMS/Zhuang.UPMS.WebMvc/Areas/Common/Controllers/SelectDialogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zhuang.UPMS.WebMvc.Areas.Common.Controllers
+{
+    public class SelectDialogController : Controller
+    {
+        // GET: Common/SelectDialog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tree()
+        {
+            return View();
+        }
+    }
+}
