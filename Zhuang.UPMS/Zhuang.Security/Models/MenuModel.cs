9998328e8bbccf801b9446da88add107de8b3234
zhuangweibin <binweizhuang@hotmail.com>
2016-08-30 14:08:39 +0800

    aaa

--- Zhuang.UPMS/Zhuang.Security/Models/MenuModel.cs
+++ Zhuang.UPMS/Zhuang.Security/Models/MenuModel.cs
@@ -12,5 +12,47 @@ namespace Zhuang.Security.Models
         public string Name { get; set; }
         public string Url { get; set; }
         public int? Seq { get; set; }
+        public IList<MenuModel> Children { get; set; }
+
+        public static List<MenuModel> ToTreeModel(List<MenuModel> lsRawModel)
+        {
+            List<MenuModel> lsResult = new List<MenuModel>();
+
+            //得到所有一级节点
+            lsResult = lsRawModel.FindAll(c =>
+            {
+                return !lsRawModel.Exists(cc => cc.Id == c.ParentId);
+            });
+
+            //定放递归函数
+            Func<string, List<MenuModel>> funRecursive = null;
+            funRecursive = (ParentId) =>
+            {
+                var Children = lsRawModel.FindAll(cc => { return cc.ParentId == ParentId; });
+
+                //如是叶子节点
+                if (Children.Count == 0)
+                {
+
+                }
+
+                Children.ForEach(c =>
+                {
+                    c.Children = funRecursive(c.Id);
+                });
+
+                return Children;
+            };
+
+            //递归处理所有子孙节点
+            lsResult.ForEach(c =>
+            {
+                c.Children = funRecursive(c.Id);
+            });
+
+            return lsResult;
+        }
+
+
     }
 }
