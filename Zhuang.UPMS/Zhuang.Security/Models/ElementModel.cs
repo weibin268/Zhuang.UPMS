42ecba0e039035af82a68dc4e1eb16cfd6019b3c
zhuangweibin <binweizhuang@hotmail.com>
2016-08-30 14:45:56 +0800

    aaa

---
+++ Zhuang.UPMS/Zhuang.Security/Models/ElementModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zhuang.Security.Models
+{
+    public class ElementModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Role { get; set; }
+    }
+}
